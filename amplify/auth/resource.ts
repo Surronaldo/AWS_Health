43951c39c366e96728682bee0cf77821a4fac681
Surronaldo <surronaldo17@gmail.com>
2025-11-06 03:25:01 +0100

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,13 @@
+// amplify/auth/resource.ts
 import { defineAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: true, // email-based sign-in
+  },
+  userPoolGroups: {
+    Doctors: {},  // assign doctors here
+    Patients: {}, // assign patients here
   },
 });
+
